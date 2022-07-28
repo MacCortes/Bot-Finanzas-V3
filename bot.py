b0f1762406ac89c0b6beeeff4c6cd29003e7c8b4
Heryc Cortés <heryccortes@gmail.com>
2022-07-28 15:47:52 -0500

    added command last n

--- bot.py
+++ bot.py
@@ -37,6 +37,20 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 def hello(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Hi, I'm your financial bot, I'll help you keep track of your expenses!")
 
+### command: /last_n
+def last_n(update: Update, context: CallbackContext):
+    """Generates an image with the last n transactions"""
+    msg = update.message.text
+
+    try:
+        n = min(int(msg.split()[1]), transactions.shape[0])
+        saves_png(transactions.tail(n), 'last_n', 'img/')
+        update.message.bot.send_photo(chat_id=update.effective_chat.id, photo=open('img/last_n.png', 'rb'))
+    except ValueError:
+        update.message.reply_text("Sorry, there was a problem with the number of rows")
+    except:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="There was an error, we'll work into it")
+
 
 ##### Bot configuration
 
@@ -48,6 +62,7 @@ disp = upd.dispatcher
 
 ### add handlers to the dispatcher
 disp.add_handler(CommandHandler('hello', hello))
+disp.add_handler(MessageHandler(Filters.regex('^(L|l)ast *') & (~Filters.command), last_n))
 
 ### start polling
 upd.start_polling()
