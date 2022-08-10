0c33bc3428ec831aa8f68e0e2bb845b8f7f22dc3
Heryc Cortés <heryccortes@gmail.com>
2022-08-10 15:20:44 -0500

    adjustment in date format

--- bot.py
+++ bot.py
@@ -5,8 +5,13 @@ import logging
 from telegram import Update,  ReplyKeyboardMarkup, ReplyKeyboardRemove, bot_api_version
 from telegram.ext import Updater, CallbackContext, CommandHandler, MessageHandler, Filters, ConversationHandler
 import pandas as pd
+import locale
 
 ##### config
+
+# Language: spanish (Mexico) "es-MX" 
+locale.setlocale(locale.LC_ALL, 'es-MX') 
+
 # formatting the float columns as currency
 pd.options.display.float_format = '${:,.2f}'.format
 
@@ -23,6 +28,8 @@ accounts = pd.read_csv('db/cuentas.csv', encoding='utf-8')
 
 # fix format of "Cantidad" column
 transactions['Cantidad'] = transactions['Cantidad'].replace('[^-.0-9]', '', regex=True).astype(float)
+# transform column "Fecha" to datetime
+transactions["Fecha"]= pd.to_datetime(transactions["Fecha"])
 
 ##### For logging how the bot it's doing
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -37,17 +44,20 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 def hello(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Hi, I'm your financial bot, I'll help you keep track of your expenses!")
 
-### command: /last_n
+### instruction: last n (ex: last 10)
 def last_n(update: Update, context: CallbackContext):
     """Generates an image with the last n transactions"""
     msg = update.message.text
+    cols = ["Tipo", "Cuenta", "Fecha", "Cantidad", "Descripción"]
 
     try:
         n = min(int(msg.split()[1]), transactions.shape[0])
-        saves_png(transactions.tail(n), 'last_n', 'img/')
+        trans_n = transactions[cols].tail(n)
+        trans_n["Fecha"] = trans_n["Fecha"].dt.strftime('%d %B %Y')
+        saves_png(trans_n, 'last_n', 'img/')
         update.message.bot.send_photo(chat_id=update.effective_chat.id, photo=open('img/last_n.png', 'rb'))
     except ValueError:
-        update.message.reply_text("Sorry, there was a problem with the number of rows")
+        update.message.reply_text("Sorry, there was a problem with the number of rows. Please, try again!")
     except:
         context.bot.send_message(chat_id=update.effective_chat.id, text="There was an error, we'll work into it")
 
